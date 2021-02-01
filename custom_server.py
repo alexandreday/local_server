2d569496915c524c19826e5735f9d7d4a03157ac
burak-yildizoz <45322678+burak-yildizoz@users.noreply.github.com>
2021-02-01 20:16:12 +0300

    custom_server.py support multiple platforms
    main is changed to run a page in any case (hello world default)

--- custom_server.py
+++ custom_server.py
@@ -1,55 +1,50 @@
-# Run python server on a different thread
 import http.server
 import socketserver
-import sys, os, random, string, signal, subprocess
-import time, random, string
+import sys
+import os
+import random
+import string
+import time
+import platform
+import shutil
+import threading
+import subprocess
 
-# run a subprocess to create server
 def random_string(n=50):
-    return ''.join(random.choice(string.ascii_lowercase+string.digits) for _ in range(n))
+    return ''.join(random.choice(string.ascii_lowercase + string.digits)
+                   for _ in range(n))
 
-def main():
-    n_arg = len(sys.argv)
-    task = sys.argv[1]
-    
-    if task == "1":
-        run_cp_kill()
-    elif task == "2":
-        run_local_server(port = 8499)
+def open_file(filename):
+    if platform.system() == 'Darwin':       # macOS
+        subprocess.call(('open', filename))
+    elif platform.system() == 'Windows':    # Windows
+        os.startfile(filename)
+    else:                                   # linux variants
+        subprocess.call(('xdg-open', filename))
 
-def run_cp_kill():
-    html_page = "index.html"
-
+def run_cp_kill(html_page):
     # Run local server
-    random_tag = random_string()
-    tmp_file = html_page.strip(".html") +"_"+ random_tag + ".html"
-    os.system('cp %s %s'%(html_page,tmp_file))
-
-    # Runs other process in the background
-    p = subprocess.Popen('python custom_server.py 2'.split(" "))
-
+    tmp_file = html_page.strip('.html') + '_' + random_string() + '.html'
+    shutil.copyfile(html_page, tmp_file)
+    # Run python server on a different thread
+    threading.Thread(target=run_local_server, daemon=True).start()
     time.sleep(0.5) # wait for server to launch
-    url = "http://localhost:8499/%s"%tmp_file
-    os.system('open %s'%url)
+    url = 'http://localhost:8499/%s' % tmp_file
+    open_file(url)
     time.sleep(0.5)
-    os.system('rm %s'%tmp_file)
-
-    # Now kill serverls
-    kill_local_server(p.pid)
+    os.remove(tmp_file)
 
 def run_local_server(port = 8000):
+    # https://stackoverflow.com/questions/15260558/python-tcpserver-address-already-in-use-but-i-close-the-server-and-i-use-allow
     socketserver.TCPServer.allow_reuse_address = True # required for fast reuse ! 
-    """
-    Check out :
-    https://stackoverflow.com/questions/15260558/python-tcpserver-address-already-in-use-but-i-close-the-server-and-i-use-allow
-    """
     Handler = http.server.SimpleHTTPRequestHandler
-    httpd = socketserver.TCPServer(("", port), Handler)
-    print("Creating server at port", port)
+    httpd = socketserver.TCPServer(('', port), Handler)
+    print('Creating server at port', port)
     httpd.serve_forever()
 
-def kill_local_server(pid):
-    test = os.kill(pid, signal.SIGTERM) # kills subprocess, allowing clean up/clearing cache
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    html_page = 'index.html'
+    if len(sys.argv) == 2:
+        html_page = str(sys.argv[1])
+    print(html_page)
+    run_cp_kill(html_page)
